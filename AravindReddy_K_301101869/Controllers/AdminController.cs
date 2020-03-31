01550c445d48ba62f9cb3dee16d3065c238eab04
Eunbi <eunbibiseo@gmail.com>
2020-03-31 19:35:11 -0400

    Add login/logout Test ID: admin Test PW: Secret123$

---
+++ AravindReddy_K_301101869/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AravindReddy_K_301101869.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AravindReddy_K_301101869.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        private IClubRepository repository;
+        public AdminController(IClubRepository repo) { repository = repo; }
+        public ViewResult Index() => View(repository.clubfromdb);
+    }
+}
---
+++ AravindReddy_K_301101869/Models/AppIdentityDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AravindReddy_K_301101869.Models
+{
+    public class AppIdentityDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : base(options) { }
+    }
+}
